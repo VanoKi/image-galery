5f685aa070db570deacee5e394753cc3235c09bf
VanoKi <kirsvano@gmail.com>
2023-09-28 21:23:45 +0300

    feat: apply D.R.Y.

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ const url = 'https://api.thecatapi.com/v1/images/search?size=full',
 
 async function getData() {
   const response = await fetch(url);
-  // console.log(response)
   const data = await response.json();
-  // const data = response.url
-  // console.log(data[0].url);
   return data[0].url
 }
 
-btn.addEventListener('click', async () => {
-  const imgUrl = await getData();
-  img.src = imgUrl
-})
+async function updateImage(){
+  img.src = await getData()
+}
 
-img.addEventListener('click', async () => {
-  const imgUrl = await getData();
-  img.src = imgUrl
-})
+// btn.addEventListener('click', async () => {
+//   const imgUrl = await getData();
+//   img.src = imgUrl
+// })
+
+// img.addEventListener('click', async () => {
+//   const imgUrl = await getData();
+//   img.src = imgUrl
+// })
+
+btn.addEventListener('click', updateImage);
+btn.addEventListener('click', updateImage)
