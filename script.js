815b47e43fcd610452358bc800f7c4ae7e5a1bed
VanoKi <kirsvano@gmail.com>
2023-09-30 23:47:21 +0300

    feat: add items to modal window

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_KEY = 'd25525bd-c6e0-4592-a65e-cf58e952d88b'
       API_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
       API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+      API_FILM_ID = `https://kinopoiskapiunofficial.tech/api/v2.2/films/`
 
 getMovies(API_URL)
 
@@ -75,20 +76,27 @@ form.addEventListener('submit', (e) => {
 const modalEl = document.querySelector('.modal');
 
 async function openModal(id){
+  const resp = await fetch(API_FILM_ID + id, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': API_KEY,
+    },
+  });
+  const respData = await resp.json();
   modalEl.classList.add('modal--show')
   modalEl.innerHTML = `
   <div class="modal__card">
-  <img src="" alt="" class="modal__movie-backdrop">
+  <img src="${respData.posterUrl}" alt="" class="modal__movie-backdrop">
   <h2>
-    <span class="modal__movie-title">title</span>
-    <span class="modal__movie-release-year">year</span>
+    <span class="modal__movie-title">${respData.nameRu}</span>
+    <span class="modal__movie-release-year">${respData.year}</span>
   </h2>
   <ul class="modal__movie-info">
     <div class="loader"></div>
-    <li class="modal__movie-genre">genre</li>
-    <li class="modal__movie-runtime">runtime</li>
-    <li>site: <a href="" class="modal__movie-site"></a></li>
-    <li class="modal__movie-overview">overview</li>
+    <li class="modal__movie-genre">${respData.genres.map(genre => ` ${genre.genre}`)}</li>
+    <li class="modal__movie-runtime">runtime: ${respData.filmLength}</li>
+    <li>site: <a href="${respData.webUrl}" class="modal__movie-site">${respData.webUrl}</a></li>
+    <li class="modal__movie-overview">${respData.description}</li>
   </ul>
   <button type="button" class="modal__btn_close">Close</button>
   </div>
@@ -100,3 +108,17 @@ async function openModal(id){
 function closeModal(){
   modalEl.classList.remove('modal--show');
 }
+
+window.addEventListener('click', (e) => {
+  // console.log(e.target)
+  if (e.target === modalEl) {
+    closeModal()
+  }
+})
+
+window.addEventListener('keydown', (e) =>{
+  // console.log(e.keyCode)
+  if (e.keyCode === 27) {
+    closeModal()
+  }
+})
