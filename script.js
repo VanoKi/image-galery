8e54ae0c48a62d2d8e9a443b42925a24e2cae339
VanoKi <kirsvano@gmail.com>
2023-10-01 14:36:10 +0300

    init: add markup, stylesm, js

--- script.js
+++ script.js
@@ -1,127 +1,30 @@
-const API_KEY = 'd25525bd-c6e0-4592-a65e-cf58e952d88b'
-      API_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
-      API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
-      API_FILM_ID = `https://kinopoiskapiunofficial.tech/api/v2.2/films/`
-      body = document.querySelector('body')
-
-getMovies(API_URL)
-
-async function getMovies(url){
-  const resp = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': API_KEY,
-    },
-  });
-  const respData = await resp.json();
-  // console.log(respData)
-  showMovies(respData)
+async function getData() {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await resp.json()
+  return data
 }
 
-function showMovies(data){
-  const moviesEl = document.querySelector('.movies');
-
-  document.querySelector('.movies').innerHTML = ''
-
-  data.films.forEach((movie) => {
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
-    movieEl.innerHTML = `
-    <div class="movie-cover-inner">
-            <img
-            src="${movie.posterUrlPreview}"
-            alt="movie_cover"
-            class="movie-cover-img">
-            <div class="movie-cover-darkened"></div>
-        </div>
-        <div class="movie-info">
-          <div class="movie-title">${movie.nameRu}</div>
-          <div class="movie-category">${movie.genres.map(genre => ` ${genre.genre}`)}</div>
-          <div class="movie-title">${movie.countries.map(country => ` ${country.country}`)}</div>
-          <div class=movie-title>${movie.year}</div>
-          ${movie.rating !== null && movie.rating !== undefined && movie.rating !== `null` &&
-          `
-          <div class="movie-average movie-average-${getClassByRate(movie.rating)}">${movie.rating}</div>
-          `
-          }
-        </div>`;
-        movieEl.addEventListener('click', () => openModal(movie.filmId))
-        moviesEl.appendChild(movieEl);
-  });
-}
-
-function getClassByRate(vote){
-  if (vote >= 7) {
-    return 'green'
-  }else if (vote >= 5){
-    return 'orange'
-  }else {
-    return 'red'
+async function main(){
+  const postsData = await getData();
+  let currentPage = 1;
+  let rows = 10
+  function displayList(arrData, rowPerPage, page){
+    const postsEl = document.querySelector('.posts')
+          start = rowPerPage * page;
+          end = start + rowPerPage;
+          paginatedData = arrData.slice(start, end);
+    paginatedData.forEach(el => {
+      const postEl = document.createElement('div')
+      postEl.classList.add('post')
+      postEl.innerText = `${el.title}`
+      postsEl.appendChild(postEl)
+    });
   }
+  function displayPagi(){}
+  function displayPagiBtn(){}
+
+  displayList(postsData, rows, currentPage)
+
 }
 
-const form = document.querySelector('form')
-      search = document.querySelector('.header-search')
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
-  if (search.value) {
-    getMovies(apiSearchUrl)
-    search.value ='' //удаляем поисковый запрос
-  }
-})
-
-//Modal
-
-const modalEl = document.querySelector('.modal');
-
-async function openModal(id){
-  const resp = await fetch(API_FILM_ID + id, {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': API_KEY,
-    },
-  });
-  const respData = await resp.json();
-  modalEl.classList.add('modal--show')
-  modalEl.innerHTML = `
-  <div class="modal__card">
-  <img src="${respData.posterUrl}" alt="" class="modal__movie-backdrop">
-  <h2>
-    <span class="modal__movie-title">${respData.nameRu}</span>
-    <span class="modal__movie-release-year">${respData.year}</span>
-  </h2>
-  <ul class="modal__movie-info">
-    <div class="loader"></div>
-    <li class="modal__movie-genre">${respData.genres.map(genre => ` ${genre.genre}`)}</li>
-    ${respData.filmLength ? `<li class="modal__movie-runtime">runtime: ${respData.filmLength}</li>` : ``}
-    <li>site: <a href="${respData.webUrl}" class="modal__movie-site">${respData.webUrl}</a></li>
-    <li class="modal__movie-overview">${respData.description}</li>
-  </ul>
-  <button type="button" class="modal__btn_close">Close</button>
-  </div>
-`
-  const btnClose = document.querySelector('.modal__btn_close')
-  btnClose.addEventListener('click', () => closeModal())
-  body.classList.add('stop-scroll')
-}
-
-function closeModal(){
-  modalEl.classList.remove('modal--show');
-  body.classList.remove('stop-scroll')
-}
-
-window.addEventListener('click', (e) => {
-  // console.log(e.target)
-  if (e.target === modalEl) {
-    closeModal()
-  }
-})
-
-window.addEventListener('keydown', (e) =>{
-  // console.log(e.keyCode)
-  if (e.keyCode === 27) {
-    closeModal()
-  }
-})
+main()
