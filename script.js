53c5cc9c5099528fc567ae5788b32927f3062db5
VanoKi <kirsvano@gmail.com>
2023-10-01 15:30:33 +0300

    feat: pagination

--- script.js
+++ script.js
@@ -9,21 +9,49 @@ async function main(){
   let currentPage = 1;
   let rows = 10
   function displayList(arrData, rowPerPage, page){
-    const postsEl = document.querySelector('.posts')
-          start = rowPerPage * page;
+    const postsEl = document.querySelector('.posts');
+    page --;
+    postsEl.innerHTML = ''
+    const start = rowPerPage * page;
           end = start + rowPerPage;
           paginatedData = arrData.slice(start, end);
     paginatedData.forEach(el => {
-      const postEl = document.createElement('div')
-      postEl.classList.add('post')
-      postEl.innerText = `${el.title}`
-      postsEl.appendChild(postEl)
+      const postEl = document.createElement('div');
+      postEl.classList.add('post');
+      postEl.innerText = `${el.title}`;
+      postsEl.appendChild(postEl);
     });
   }
-  function displayPagi(){}
-  function displayPagiBtn(){}
+  function displayPagi(arrData, rowPerPage){
+    const pagiEl = document.querySelector('.pagi');
+          pagesCount = Math.ceil(arrData.length / rowPerPage);
+          ulEl = document.createElement('ul');
+          ulEl.classList.add('pagi__list');
+    for (let index = 0; index < pagesCount; index++) {
+      const liEL = displayPagiBtn(index + 1)
+      ulEl.appendChild(liEL)
+    }
+    pagiEl.appendChild(ulEl)
+  }
+  function displayPagiBtn(page){
+    const liEl = document.createElement('li')
+    liEl.classList.add('pagi__item')
+    liEl.innerText = page
+    if (currentPage == page) {
+      liEl.classList.add('pagi__item--active')
+    }
+    liEl.addEventListener('click', () => {
+      currentPage = page;
+      displayList(postsData, rows, currentPage)
+      let currentItemLi = document.querySelector('li.pagi__item--active')
+      currentItemLi.classList.remove('pagi__item--active');
+      liEl.classList.add('pagi__item--active')
+  })
+    return liEl
+  }
 
-  displayList(postsData, rows, currentPage)
+  displayList(postsData, rows, currentPage);
+  displayPagi(postsData, rows)
 
 }
 
